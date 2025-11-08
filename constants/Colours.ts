1f50a955d9ef45245834f5d302af1190dd996b3d
JackRiiley <133687746+JackRiiley@users.noreply.github.com>
2025-11-08 18:12:36 +0000

    Creating colour schema

---
+++ constants/Colours.ts
@@ -0,0 +1,19 @@
+// A basic color palette for our app
+
+const tintColorLight = '#007AFF'; // A nice blue for buttons, XP bar
+
+export default {
+  light: {
+    text: '#171717', // Primary text
+    textSecondary: '#525252', // Lighter text
+    background: '#f4f4f8', // Light gray background
+    card: '#ffffff', // Card backgrounds (like TaskItem)
+    tint: tintColorLight,
+    border: '#e5e5e5', // Light borders
+    placeholder: '#a3a3a3', // Placeholder text
+  },
+  // We can add a dark mode palette here later
+  dark: {
+    // ... coming soon
+  },
+};
